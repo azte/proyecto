7a16b788b17534467b3c8adca99eba4afbb4de80
azte <cuellar.uk@gmail.com>
2014-04-15 12:49:35 -0500

    PLUS & MINUS
    Formulario de Enlaces

--- app/views.py
+++ app/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from datetime import datetime
 from django.shortcuts import render_to_response
 from models import *
+from django.shortcuts import get_object_or_404
+from forms import *
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def add(request):
+	
+	categorias = Categoria.objects.all()
+	if request.method == "POST":
+		form = EnlaceForm(request.POST)
+		if form.is_valid():
+			enlace.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = EnlaceForm()
+	template = "form.html"
+	return render_to_response(template,context_instance = RequestContext(request,locals()))
 
 def home(request):
 	categorias = Categoria.objects.all()
@@ -12,6 +30,30 @@ def home(request):
 	template = "index.html"
 	#diccionario = {"categorias":categorias,"enlaces":enlaces} En lugar de locals
 	return render_to_response(template,locals())
+
+
+def categoria(request, id_categoria):
+	categoria = Categoria.objects.all()
+	cat= get_object_or_404(Categoria,pk = id_categoria)
+
+	cat = Categoria.objects.get(pk=id_categoria)
+	enlaces = Enlace.objects.filter(categoria = cat)
+	template = "index.html"
+	return render_to_response(template,locals())
+ 	
+@login_required
+def minus(request, id_enlace):
+	enlace = Enlace.objects.get(pk=id_enlace)
+	enlace.votos = enlace.votos -1
+	enlace.save()
+	return HttpResponseRedirect("/")
+
+@login_required
+def plus(request, id_enlace):
+	enlace = Enlace.objects.get(pk=id_enlace)
+	enlace.votos = enlace.votos +1
+	enlace.save()
+	return HttpResponseRedirect("/")
 
 
 
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -6,6 +6,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'app.views.home', name='home'),
+    url(r'^plus/(\d+)$','app.views.plus', name='plus'),
+    url(r'^minus/(\d+)$','app.views.minus', name='minus'),
+    url(r'^add/$','app.views.add', name='add'),
+
+   
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
